2cde393c91c39181dbe0b2f060d8f19f312dcd03
Stephen Pennell <stephenpe@knowclassic.com>
2016-04-14 22:48:03 -0400

    Updated vhost version

--- vhost.rb
+++ vhost.rb
@@ -2,7 +2,7 @@ class Vhost < Formula
   desc "Manage your Virtual Hosts"
   homepage "https://github.com/steyep/vhost"
   head "https://github.com/steyep/vhost.git", :using => :git, :branch => 'master'
-  url 'https://github.com/steyep/vhost/archive/1.0.1.tar.gz'
+  url 'https://github.com/steyep/vhost/archive/1.1.0.tar.gz'
   sha256 "0b23e347f6d068c0248fd5fde43822f882502210b9714da92647c553aef5d0ed"
 
   def install
